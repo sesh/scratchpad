49b05f26ea0bab370d1bc3069abb3e58649c754f
Brenton Cleeland <brenton@brntn.me>
2020-10-16 18:29:14 +1100

    Shuffle

--- app.js
+++ app.js
@@ -69,9 +69,34 @@ let continueListOnNewline = (scratchpad) => {
     }
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 let sortLines = (scratchpad) => {
     let lines = scratchpad.value.split("\n");
-    lines.sort();
+    shuffleArray(lines);
+    scratchpad.value = lines.join("\n");
+}
+
+let shuffleLines = (scratchpad) => {
+    let lines = scratchpad.value.split("\n");
+    lines = shuffle(lines);
     scratchpad.value = lines.join("\n");
 }
 
@@ -285,6 +310,11 @@ let openDismissablePanel = (id) => {
         {
             "name": "sort",
             "action": sortLines,
+            "description": "Sort all lines in the scratchpad alphabetically"
+        },
+        {
+            "name": "shuffle",
+            "action": shuffleLines,
             "description": "Sort all lines in the scratchpad alphabetically"
         }
     ];
